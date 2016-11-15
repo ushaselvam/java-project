970c951fcf6df357118602d7fbc15548de116b5b
ushaselvam <ushaselvam1995@gmail.com>
2016-11-15 14:49:47 +0530

    Create Rectangle.java

---
+++ package/area/Rectangle.java
@@ -0,0 +1,8 @@
+package rectangle;
+public class Rectangle {
+	public double length;
+	public double breadth;
+	public void area() {
+		System.out.println("AREA OF RECTANGLE "+(length*breadth));
+	}
+}
